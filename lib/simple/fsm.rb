ba8cc0deda634daf58675fa5a9b8d1bff2046ec2
Polina Miftakhudinova <rambeeech@gmail.com>
2021-12-20 18:12:53 +0400

    Initial commit

---
+++ lib/simple/fsm.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "fsm/version"
+
+module Simple
+  module Fsm
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/simple/fsm/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Simple
+  module Fsm
+    VERSION = "0.1.0"
+  end
+end
